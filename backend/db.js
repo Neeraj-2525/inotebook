2c9582b353ae3eb266d29d5e34308c4a92a1fdbb
Neeraj-2525 <neerajkumara2525@gmail.com>
2023-08-13 14:56:54 +0530

    added cloud database link

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv")
 dotenv.config({ path: './backend/config.env' });
 const cron = require('./scheduledTasks.js');
 
-const mongoURI = "mongodb://localhost:27017/inotebookBackend";
-// const atlasURI = process.env.DATABASE
+// const mongoURI = "mongodb://localhost:27017/inotebookBackend";
+const atlasURI = process.env.DATABASE
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI)
+    mongoose.connect(atlasURI)
     .then(() => console.log("Successfully Connected to cloud db"))
     .catch((e) => console.log("Refused to connect \n" + e))
 }
