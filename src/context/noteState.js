a8067e27baa857fef83bdf85b9c6fe1f4e2d42bc
Neeraj-2525 <neerajkumara2525@gmail.com>
2023-08-14 15:13:22 +0530

    added database ip address to access it

--- src/context/noteState.js
+++ src/context/noteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "" || "http://localhost:5000";
+  const host = "0.0.0.0/0" || "http://localhost:5000";
   const notesInitial = []
   const tagsInitial = []
   const [notes, setNotes] = useState(notesInitial)
